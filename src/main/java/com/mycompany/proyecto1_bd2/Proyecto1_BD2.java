33ddd446e2854a037e9c58a4b0a63e187dbf6c86
CesarTecun <ctecunl1@miumg.edu.gt>
2025-04-07 20:14:41 -0600

    Initial commit

---
+++ src/main/java/com/mycompany/proyecto1_bd2/Proyecto1_BD2.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.proyecto1_bd2;
+
+/**
+ *
+ * @author cesar
+ */
+public class Proyecto1_BD2 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
